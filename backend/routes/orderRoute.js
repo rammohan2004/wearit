3f67caf8ff950b56e31bd2fc4ec5c35ecc36ebda
Ram mohan <ramamohanraobotla12@gmail.com>
2024-09-25 21:57:33 +0530

    orders api

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import {placeOrder,placeOrderRazorPay,placeOrderStripe,allOrders,userOrders,updateStatus} from '../controllers/orderController.js' 
+import adminAuth from '../middleware/adminAuth.js'
+import authUser from '../middleware/auth.js'
+const orderRouter = express.Router();
+
+// admin features
+orderRouter.post('/list',adminAuth,allOrders)
+orderRouter.post('/status',adminAuth,updateStatus)
+
+// payment Features
+
+orderRouter.post('/place',authUser,placeOrder)
+orderRouter.post('/stripe',authUser,placeOrderStripe)
+orderRouter.post('/razorpay',authUser,placeOrderRazorPay)
+
+
+// User Feature
+
+orderRouter.post('/userorders',authUser,userOrders)
+
+
+export default orderRouter;
