56d8aab5a7526f92edcf5052f330c84f71d086a0
Ram mohan <ramamohanraobotla12@gmail.com>
2024-09-26 14:14:43 +0530

    payment integration

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder,placeOrderRazorPay,placeOrderStripe,allOrders,userOrders,updateStatus} from '../controllers/orderController.js' 
+import {placeOrder,placeOrderRazorPay,placeOrderStripe,allOrders,userOrders,updateStatus, verifyStripe} from '../controllers/orderController.js' 
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 const orderRouter = express.Router();
@@ -19,5 +19,8 @@ orderRouter.post('/razorpay',authUser,placeOrderRazorPay)
 
 orderRouter.post('/userorders',authUser,userOrders)
 
+//verify payment
+
+orderRouter.post('/verifyStripe',authUser,verifyStripe);
 
 export default orderRouter;
