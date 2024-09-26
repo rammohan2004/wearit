479c8378f9fbb78d75bc80cac0374a1101d1aec5
Ram mohan <ramamohanraobotla12@gmail.com>
2024-09-26 14:35:56 +0530

    payment integration

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -74,7 +74,7 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
-import wearit from './wearit.png'
+import wearit from './Wearit.png'
 
 export const assets = {
     logo,
